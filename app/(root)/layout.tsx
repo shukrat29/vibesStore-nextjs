52faa1deb36e3745eec4d62e7210de57851b499b
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-17 22:25:29 +1100

    build header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/shared/header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
-      ROOT
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
     </div>
   );
