b764f0ece4b73a159bab41b3e32a4027590479ba
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-20 21:01:54 +1100

    design product image page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -19,9 +20,7 @@ const ProductDetailsPage = async (props: {
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* images column */}
           <div className="col-span-2">
-            {
-              // Images Component
-            }
+            <ProductImages images={product.images} />
           </div>
           {/* Details Column */}
           <div className="col-span-2">
