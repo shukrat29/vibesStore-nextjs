505ba83ca22ca641454f60123431e580366c56c3
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-21 12:54:23 +1100

    built signup page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -14,3 +14,10 @@ export const signInDefaultValues = {
   email: "",
   password: "",
 };
+
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
