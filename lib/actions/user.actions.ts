7f4e44a942626cac157963202c1bc5d26062fc19
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-21 11:08:12 +1100

    completed signin page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { signInFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
-import { isRedirectError } from "next/dist/client/components/redirect";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
