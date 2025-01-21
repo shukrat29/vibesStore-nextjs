396bc1a7d2cb0334258f46e50a0f6c786f74834f
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-21 13:19:47 +1100

    handle signup error

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,29 @@ export function formatNumberWithDecimal(num: number): string {
 
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
+
+// Format errors // sign up error handling
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatError(error: any) {
+  if (error.name === "ZodError") {
+    // Handle Zod error
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Handle prisma error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exist`;
+  } else {
+    // Handle other error
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
