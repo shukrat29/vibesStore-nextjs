b03e414eb88bdb4c14e1f1491ed0852be81abf6b
Shukra Tamang <shukratamang.npl@gmail.com>
2025-02-02 20:08:31 +1100

    created monthly sales chart

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -13,6 +13,7 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
 import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import Charts from "./charts";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -83,7 +84,9 @@ const AdminOverviewPage = async () => {
           <CardHeader>
             <CardTitle>Overview</CardTitle>
           </CardHeader>
-          <CardContent>{/* Chart here */}</CardContent>
+          <CardContent>
+            <Charts data={{ salesData: summary.salesData }} />
+          </CardContent>
         </Card>
         <Card className="col-span-3">
           <CardHeader>
