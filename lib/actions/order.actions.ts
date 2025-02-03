308d36a337c68446198d98da226a82304f0f9f8b
Shukra Tamang <shukratamang.npl@gmail.com>
2025-02-03 19:20:54 +1100

    fetched all orders in admin dashboard

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -340,3 +340,26 @@ export async function getOrderSummary() {
     salesData,
   };
 }
+
+// Get all orders for admin dashboard
+export async function getAllOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.order.findMany({
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+    include: { user: { select: { name: true } } },
+  });
+
+  const dataCount = await prisma.order.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
