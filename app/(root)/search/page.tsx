76b6c0963b87b7e80aa67fdd467b63698c82d4b5
Shukra Tamang <shukratamang.npl@gmail.com>
2025-02-14 20:48:59 +1100

    filtered product by category and price

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,5 +1,32 @@
 import ProductCard from "@/components/shared/product/product-card";
-import { getAllProducts } from "@/lib/actions/product.actions";
+import {
+  getAllCategories,
+  getAllProducts,
+} from "@/lib/actions/product.actions";
+import Link from "next/link";
+
+const prices = [
+  {
+    name: "$1 to $50",
+    value: "1-50",
+  },
+  {
+    name: "$51 to $100",
+    value: "51-100",
+  },
+  {
+    name: "$101 to $200",
+    value: "101-200",
+  },
+  {
+    name: "$201 to $500",
+    value: "201-550",
+  },
+  {
+    name: "$501 to $1000",
+    value: "501-1000",
+  },
+];
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -20,6 +47,31 @@ const SearchPage = async (props: {
     page = "1",
   } = await props.searchParams;
 
+  // Construct filter url
+  const getFilterUrl = ({
+    c,
+    p,
+    s,
+    r,
+    pg,
+  }: {
+    c?: string;
+    p?: string;
+    s?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (s) params.sort = s;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   const products = await getAllProducts({
     query: q,
     category,
@@ -29,9 +81,63 @@ const SearchPage = async (props: {
     page: Number(page),
   });
 
+  const categories = await getAllCategories();
+
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* Filters */}</div>
+      <div className="filter-links">
+        {/* Filters */}
+        {/* Category links */}
+        <div className="text-xl mb-2 mt-3">Department</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${
+                  (category === "all" || category === "") && "font-bold"
+                }`}
+                href={getFilterUrl({ c: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {categories.map((x) => (
+              <li key={x.category}>
+                <Link
+                  className={`${category === x.category && "font-bold"}`}
+                  href={getFilterUrl({ c: x.category })}
+                >
+                  {x.category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Price links */}
+        <div className="text-xl mb-2 mt-3">Price</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${price === "all" && "font-bold"}`}
+                href={getFilterUrl({ p: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {prices.map((p) => (
+              <li key={p.value}>
+                <Link
+                  className={`${price === p.value && "font-bold"}`}
+                  href={getFilterUrl({ p: p.value })}
+                >
+                  {p.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div className="md:col-span-4 space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && <div>Product not found</div>}
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,6 +5,7 @@ import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import { insertProductSchema, updateProductSchema } from "../validators";
 import { z } from "zod";
+import { Prisma } from "@prisma/client";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -49,8 +50,49 @@ export async function getAllProducts({
   rating?: string;
   sort?: string;
 }) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
+  // Category filter
+  const categoryFilter = category && category !== "all" ? { category } : {};
+
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== "all"
+      ? {
+          price: {
+            gte: Number(price.split("-")[0]),
+            lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+
+  // Rating filter
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
+
   const data = await prisma.product.findMany({
     orderBy: { createdAt: "desc" },
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
     skip: (page - 1) * limit,
     take: limit,
   });
